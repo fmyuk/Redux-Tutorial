fb9e3df120fa68581ef75ff998969e3a53a00e04
fumi uk <dcfumiya.238@gmail.com>
2020-11-27 07:37:07 +0900

    redux

--- src/components/GoogleBooks/index.tsx
+++ src/components/GoogleBooks/index.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
-import { VolumeList } from './models/Volume';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { SearchResult } from './SearchResult';
+
+import { VolumeList } from '../../models/Volume';
+import { State } from '../../reducers';
+import { GoogleBooksActions } from '../../actions/googleBooks';
 
 const searchGoogleBooks = async (searchString: string) => {
-  const url = "https://www.googleapis.com/books/v1/volumes";
+  const url = 'https://www.googleapis.com/books/v1/volumes';
   const params = { q: searchString };
   try {
     const response = await axios.get(url, { params });
@@ -14,21 +20,23 @@ const searchGoogleBooks = async (searchString: string) => {
   }
 };
 
-export const Otameshi: React.FC = () => {
+export const GoogleBooks: React.FC = () => {
   const [searchString, changeSearchString] = useState("");
-  const [searchResult, changeSearchResult] = useState<VolumeList>(new VolumeList());
-  
+
+  const { volumeList } = useSelector((state: State) => ({ volumeList: state.googleBooks.volumeList }));
+
+  const dispatch = useDispatch();
+
   const handleOnSearchButton = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    // form要素のbuttonのsubmitを止める
     event.preventDefault();
     const result = await searchGoogleBooks(searchString);
     if (result.isSuccess) {
-      changeSearchResult(VolumeList.fromResponse(result.data));
+      dispatch(GoogleBooksActions.setVolumes(VolumeList.fromResponse(result.data)));
     } else {
       window.alert(String(result.error));
     }
   };
-  
+
   return (
     <Wrapper>
       <Body>
@@ -39,24 +47,20 @@ export const Otameshi: React.FC = () => {
             検索
           </SearchButton>
         </SearchForm>
-        {searchResult.kind && (
-          <ResultContent>
-            {searchResult.items.map(item => {
-              return <ResultTitle key={item.id}>{item.volumeInfo.title}</ResultTitle>;
-            })}
-          </ResultContent>
-        )}
+        {volumeList.kind && <SearchResult volumeList={volumeList} />}
       </Body>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 20px;
 `;
-const Body = styled.div``;
+const Body = styled.div`
+  max-width: 600px;
+`;
 const Title = styled.h1`
   font-size: 24px;
   font-weight: bold;
@@ -90,15 +94,5 @@ const SearchButton = styled.button`
   &:disabled {
     background-color: #bfbfbf;
     cursor: not-allowed;
-  }
-`;
-const ResultContent = styled.div`
-  margin-top: 20px;
-`;
-const ResultTitle = styled.div`
-  padding: 10px 0;
-  border-bottom: 1px solid;
-  &:first-of-type {
-    border-top: 1px solid;
   }
 `;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createBrowserHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+
+import { configureStore, history } from './configureStore';
 
 import routes from './routes';
-import * as serviceWorker from './serviceWorker';
 
-const history = createBrowserHistory();
+const store = configureStore();
 
-ReactDOM.render(<Router history={history}>{routes}</Router>, document.getElementById('root'));
-
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>{routes}</ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
